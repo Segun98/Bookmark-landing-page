e0a3a4683f78e371d8be3d8fa503d91ac9c10355
Segun98 <53624548+Segun98@users.noreply.github.com>
2019-11-13 10:44:24 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+var button = document.getElementById('submit').addEventListener("click", ValidateEmail)
+var field = document.getElementById('input')
+
+
+function ValidateEmail() {
+
+    var error = document.querySelector('.error');
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field.value)) {
+        return (true)
+    } else {
+        error.style.display = "block";
+    }
+
+
+}
+
+// var viewMode = getCookie("view-mode");
+// if(viewMode == "desktop"){
+//     viewport.setAttribute('content', 'width=1024');
+// }else if (viewMode == "mobile"){
+//     viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+// }
+var sb = document.getElementById('sb').addEventListener('click', displayf)
+var ss = document.getElementById('ss').addEventListener('click', displayfs)
+var es = document.getElementById('es').addEventListener('click', displayfe)
+
+function displayf () { 
+
+    var sharing = document.getElementById('sharing')
+        sharing.classList.remove('displayfeature');
+        
+    var searching = document.getElementById('searching')
+        searching.classList.remove('displayfeature')  
+        
+    var bookmark = document.getElementById('bookmarking')
+    bookmark.classList.add('displayfeature')
+          
+
+}
+function displayfs () {
+    var bookmark = document.getElementById('bookmarking')
+    bookmark.classList.remove('displayfeature') 
+
+    var sharing = document.getElementById('sharing')
+        sharing.classList.remove('displayfeature');
+
+    var searching = document.getElementById('searching')
+        searching.classList.add('displayfeature')
+
+
+}
+
+function displayfe () {
+    var bookmark = document.getElementById('bookmarking')
+    bookmark.classList.remove('displayfeature') 
+    
+    var searching = document.getElementById('searching')
+        searching.classList.remove('displayfeature')
+
+        var sharing = document.getElementById('sharing')
+        sharing.classList.add('displayfeature');
+}
